af1df8c5c1940134ca4d2a2552ae06a9c2ae3d0c
berg <paulemberg@gmail.com>
2022-09-07 19:12:36 -0300

    initial commit

--- popup.js
+++ popup.js
@@ -1,27 +1,17 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
-let color = chrome.storage.sync.get("color");
-let name = document.getElementById('txtName');
+var btnEnviar = document.getElementById("btnEnviar");
 
-changeColor.addEventListener("click", async () => {
+async function sendToNotion() {
+  //TODO:
+}
 
-	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-	//console.log(name.value + '\n' + tab.url);	
-	console.log(tab.url);	
-	console.log(name.value);
+btnEnviar.addEventListener("click", async () => {
+  var queryInfo = { active: true, currentWindow: true };
+  var tab = await chrome.tabs.query(queryInfo);
+  var url = tab[0].url;
+  var sname = document.getElementById("txtName");
 
- });
+  console.log(url);
+  console.log(sname.value);
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+  alert(`Enviado para o Notion\nurl: ${url}\nNome: ${sname.value}`);
 });
-
-
-
-
-
-
-
-
-
-
