b35237265ade78acbb1d34a71df3d3efca412a57
berg <paulemberg@gmail.com>
2022-11-05 10:25:53 -0300

    shore: change file css and separeted request to notion api

---
+++ requester.js
@@ -0,0 +1,22 @@
+import Exports from "./exportEnviroments.js";
+
+export default class RequesterNotion {
+  CallNotion(data) {
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === this.DONE) {
+        console.log(this.responseText);
+      }
+    });
+
+    var expSecret = new Exports();
+    xhr.open("POST", "https://api.notion.com/v1/pages");
+    xhr.setRequestHeader("Notion-Version", "2022-06-28");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Authorization", "Bearer " + `${expSecret.GetSecret}`);
+
+    xhr.send(data);
+  }
+}
